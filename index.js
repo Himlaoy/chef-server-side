72062fe9e7928c3faa4e10ead5ab22926438144b
himaloyash <himaloyashis98@gmail.com>
2023-05-02 23:26:14 +0600

    json pic addedd

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const recipes = require('./data/recipes.json')
 const port = 5000
 
 
 app.get('/', (req, res)=>{
-    res.send('chef is getting ready to cook')
+    res.send(recipes)
+})
+
+app.get('/recipesDetail/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    const selectedRecipes = recipes.find(recipe=>parseInt(recipe.id) === id)
+    res.send(selectedRecipes)
+    
 })
 
 app.listen(port,()=>{
